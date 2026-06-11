1dc579ab7831ace1086c7e2b6f6f1893c0ec45a0
Raheem Ajibona <ajibonaraheem@gmail.com>
2026-06-11 03:19:59 -0700

    fix dataset path for render deployment

--- model/train_model.py
+++ model/train_model.py
@@ -9,8 +9,8 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_absolute_error, r2_score
 
-DATA_PATH = "../dataset/livestock_training_data.csv"
-MODEL_DIR = "../saved_model"
+DATA_PATH = "dataset/livestock_training_data.csv"
+MODEL_DIR = "saved_model"
 os.makedirs(MODEL_DIR, exist_ok=True)
 
 df = pd.read_csv(DATA_PATH)
